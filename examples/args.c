e07bcefff123b99ee5262f1c02d2c81aa846ae24
Jaime Avilleira Garcia <j.avilleira.2019@alumnos.urjc.es>
2023-02-08 14:40:02 +0100

    02-08 examples

--- examples/args.c
+++ examples/args.c
@@ -7,6 +7,25 @@
 
 /*
     NULL = (void *) 0 ---> Puntero a cualquier cosa. Se utilizan para funciones que reservan memoria.
+
+    En las structs:
+        - typedef struct Coordenada para definir un tipo Coordenada.
+        - Si hay un puntero a coordenada:
+            Coordenada *pc;
+            Coordenada c;
+            pc = &c;
+            c.v = 3;
+            (*pc).x = 3 ===== pc->x = 3;
+
+    Memoria dinámica:
+        Importante para que tu programa pueda seguir utilizando la memoria. Liberar en cuanto se deja de usar.
+            Coordenada *pc;
+            pc = (Coordenada)malloc(sizeof(Coordenada));
+            free(pc);
+            pc = NULL; ----> Asegurarse de que nadie lo puede utilizar.
+            free(pc); ----> Liberar dos veces, el programa casca.
+        Toda la memoria global sin inicializar (variable global sin inicializar: memoria cuyo ámbito es todo el fichero desde donde se declara) se obtiene desde heap.
+
 */
 int
 main (int argc, char **argv){
