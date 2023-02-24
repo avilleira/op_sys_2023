90bcac0752d97440a905a5643c9924ef003f5289
Jaime Avilleira Garcia <j.avilleira.2019@alumnos.urjc.es>
2023-02-24 14:43:26 +0100

    Initial commit for the P2

--- p2/defenv.c
+++ p2/defenv.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
 #include <stdlib.h>
+#include <string.h>
+#include <err.h>
+
+
+void
+usage(void) {
+    fprintf(stderr, "usage: ./defenv [ENV_VAR] PROGRAM_ROUTE\n");
+    exit(EXIT_FAILURE);
+}
 
 
 int
 main (int argc, char *argv[]) {
+
+    if (argc <= 1)  {
+        usage();
+    }
+
+    switch (fork()) {
+    case -1:
+        errx(EXIT_FAILURE, "fork failed!");
+    case 0:
+        execl("/bin/ls", "ls", NULL);
+        break;
+    default:
+        printf("I am the parent\n");
+    }
+
     return(EXIT_SUCCESS);
 }
