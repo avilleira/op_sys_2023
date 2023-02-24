79a711706ef11bc7dffc1f41f642bc13ab96cd86
avilleira <j.avilleira.2019@alumnos.urjc.es>
2023-02-24 13:52:13 +0100

    Setting P1

--- p1/listcreat.h
+++ p1/listcreat.h
@@ -1,22 +1,20 @@
-
 struct Creat {
-    long long id;
-    char *specie;
-    char *color;
-    int height; //mm
-    int weight; //g
-    struct Creat* prev;
-    struct Creat* next;
+	long long id;
+	char *specie;
+	char *color;
+	int height;		//mm
+	int weight;		//g
+	struct Creat *prev;
+	struct Creat *next;
 };
-
+typedef struct Creat Creat;
 
 struct List {
-    int n_creatures;
-    struct Creat *first;
-    struct Creat *last;
+	int n_creatures;
+	struct Creat *first;
+	struct Creat *last;
 };
 
-typedef struct Creat Creat;
 typedef struct List List;
 
 // Print all the characteristics of the creature.
@@ -26,4 +24,8 @@ void print_creat(Creat *creat);
 List *create_list(void);
 
 // Checking if the list is empty.
-int is_empty(List* list);
+int is_empty(List *list);
+
+// Adding elements at the top of the list.
+void
+ add_creature_top(Creat *creat, List *list);
--- p1/main.c
+++ p1/main.c
@@ -3,17 +3,33 @@
 #include "listcreat.h"
 
 void
-usage(void) {
-    fprintf(stderr, "Usage: ./main\n");
-    exit(EXIT_FAILURE);
+usage(void)
+{
+	fprintf(stderr, "Usage: ./main\n");
+	exit(EXIT_FAILURE);
 }
 
+Creat creatures[] = {
+	{1, "meerkat", "brown", 300, 730, NULL, NULL},
+	{2, "zarigüeya", "grey", 700, 6000, NULL, NULL},
+	{3, "dog", "white", 250, 4000, NULL, NULL},
+	{4, "cat", "black", 240, 6000, NULL, NULL},
+	{5, "parakeet", "yellow", 190, 35, NULL, NULL},
+	{6, "mole", "brown", 200, 550, NULL, NULL},
+	{7, "eagle", "brown", 950, 7000, NULL, NULL},
+	{8, "dolphin", "grey", 2700, 140000, NULL, NULL},
+	{9, "horse", "white", 1700, 420000, NULL, NULL},
+	{10, "ostrich", "black", 3000, 180000, NULL, NULL}
+};
+
 int
-main (int argc, char *argv[]) {
-    if (argc != 1)
-        usage();
-    List* creature_list;
-    creature_list = create_list();
-    printf("III:%d\n", is_empty(creature_list));
-    exit(EXIT_SUCCESS);
+main(int argc, char *argv[])
+{
+	if (argc != 1)
+		usage();
+	List *creature_list;
+
+	creature_list = create_list();
+	add_creature_top(&creatures[0], creature_list);
+	exit(EXIT_SUCCESS);
 }
