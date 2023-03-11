5aad857c630389e108edccc655d674bdefb49e5c
avilleira <j.avilleira.2019@alumnos.urjc.es>
2023-03-12 00:41:19 +0100

    initial pinger
    Signed-off-by: avilleira <j.avilleira.2019@alumnos.urjc.es>

---
+++ p3/pinger.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void
+usage(void) {
+    fprintf(stderr, "usage: pinger N machine1 [machines ...]\n" );
+    exit(EXIT_FAILURE);
+}
+
+int main (int argc, char *argv[]){
+    return EXIT_SUCCESS;
+}
