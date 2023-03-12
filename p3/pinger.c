f17376c3f7fbdebb5a2ea4625c8e8e9c6ab43098
avilleira <j.avilleira.2019@alumnos.urjc.es>
2023-03-12 20:46:52 +0100

    Isanumber method finished
    Signed-off-by: avilleira <j.avilleira.2019@alumnos.urjc.es>

--- p3/pinger.c
+++ p3/pinger.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void
 usage(void) {
@@ -7,6 +8,26 @@ usage(void) {
     exit(EXIT_FAILURE);
 }
 
+int is_a_number(char *number) {
+
+    if ((atoi(number) == 0) && (strcmp(number, "0") != 0))
+        return 0;
+    else
+        return 1;
+}
+
 int main (int argc, char *argv[]){
+    
+    int deadline, pid;
+
+    if (argc < 3) {
+        usage();
+    }
+    
+    if (is_a_number(argv[1]))
+        deadline = atoi(argv[1]);
+    else
+        usage();
+
     return EXIT_SUCCESS;
 }
