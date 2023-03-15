d21ab0ab150a4503740e68878e611778410bf5b1
avilleira <j.avilleira.2019@alumnos.urjc.es>
2023-03-15 01:01:57 +0100

    Initial commit copybytes
    Signed-off-by: avilleira <j.avilleira.2019@alumnos.urjc.es>

---
+++ p4/copybytes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void
+usage (void) {
+    fprintf(stderr, "usage: ./copybytes origin dest [N]\n");
+    exit(EXIT_FAILURE);
+}
+
+
+int
+main (int argc, int argv[]) {
+    return EXIT_SUCCESS;
+}
