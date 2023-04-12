ebbc0b273d269ea7bfa73686643536a2188c5138
avilleira <j.avilleira.2019@alumnos.urjc.es>
2023-04-12 13:58:43 +0200

    Usage created
    Signed-off-by: avilleira <j.avilleira.2019@alumnos.urjc.es>

--- p7/pipeline.c
+++ p7/pipeline.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void
+usage (void)
+{
+    fprintf(stderr, "usage: ./pippeline 'COMMAND' 'COMMAND' 'COMMAND'\n");
+    exit(EXIT_FAILURE);
+}
+
 
 int
 main (int argc, char *argv[])
 {
+    if (argc != 4)
+        usage();
     return EXIT_SUCCESS;
 }
